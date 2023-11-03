7236b49235fb7d1a20511edb4cd1883ef3cb181f
Павел Бакшеев <sxeiter@mail.ru>
2023-11-03 21:28:22 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const modalTitle = document.querySelector('.modal__title');
+const modalClose = document.querySelector('.modal__close');
+const vendorCodeId = document.querySelector('.vendor-code__id');
+const modalForm = document.querySelector('.modal__close');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
+const modalTotalPrice = document.querySelector('.modal__total-price');
